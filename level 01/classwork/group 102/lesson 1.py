c994fc5e4958c95b82621467850b64bfa722f592
tika2011 <indira.gorgadze.2017@gmail.com>
2024-06-27 18:39:42 +0400

    commit

---
+++ level 01/classwork/group 102/lesson 1.py
@@ -0,0 +1,33 @@
+name = "tika"
+#name  არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"tika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "qatamadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსტანი ობიექტი
+
+name = "tika" #ეს არის str (string) ტიპის ცვლადი
+age = 13  # ეს არის int (interger)  მთელი რიცხვი
+height =155 # ეს არის float ტიპის ცვლადი (ათწილადი)
+# boolend (bool)ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False               #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly =False   # ჯავასკრიპტული camalcase
+  
+
+print(name + " " +  surname)
+
+#  სტრინგი არის რჭყალებში მოქცეული სიმბოლოები
+print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
---
+++ level 01/homework/lesson 1.py
@@ -0,0 +1,10 @@
+name = "tika"
+surname = "qatamadze"
+age = 12
+height = 155
+
+#booleand (bool)
+know_dance = True
+is_evil = False
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " +str(bool))
