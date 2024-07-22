ae20f3757acc616f785c7ac47a916f27c9ba5eca
tika2011 <indira.gorgadze.2017@gmail.com>
2024-07-23 00:56:10 +0400

    level04

---
+++ level 05/claswork/leson 05.py
@@ -0,0 +1,29 @@
+name = "Alice"
+age = 25
+height = 5.7
+weight = 60.5
+hobby = ("reading")
+
+print("Name:", name)
+print("Age: ", age)
+print("Height:", height)
+print("Weight:", weight)
+print("Hobby:", hobby)
+
+new_age = age + 1
+print("New Age:", new_age)
+
+full_height =(str) (height) + " meters "
+print("Full Height:", full_height)
+
+description = name + " is " + (str) (age )+ " years old and likes " + hobby
+print(description)
+
+new_weight = weight - 5.0
+print("New Weight:", new_weight)
+
+message = "Hello" + (str) (123)
+print(message)
+
+age_next_year = age + 1
+print("Age Next Year:", age_next_year)
