ae486a99e525135a4933e5718b1d6c07d0ec605a
tika2011 <indira.gorgadze.2017@gmail.com>
2024-07-23 12:13:56 +0400

    level05

---
+++ level 05/homework/lesson 05 .py
@@ -0,0 +1,56 @@
+name = "Tika"
+print (name) 
+
+surname = "qatamadze"
+print (surname)
+
+hight = 1,60
+print (hight)
+
+age = 12
+print (age)
+
+car = "nissan"
+print (car)
+
+flower = "rose"
+print (flower)
+
+hobby = "dance"
+print (hobby)
+
+food = "pizza"
+print (food)
+
+drink = "ice tea"
+print (drink)
+
+city = "batumi"
+print (city)
+
+print( " my name is " + name + " my surname is " + surname + " my hight is "+ str (hight) + " my age is " + str (age) +" my faworite car is " + car + 
+"my faworite flower is " + flower + " my faworite hobby  is" + hobby + "my faworite food is " + food + " my faworite drink is " + drink + "my faworite city is"
++ city )
+
+
+
+
+#02
+name + "tika"
+#name = "tika"
+print "name"
+#print (name)
+
+surname = 'qatamadze'
+#surname = "qatamadze"
+print (qatamadze)
+# print(surname)
+
+hight = "155)
+print {hight}
+
+age -(|13)
+print age
+
+flower <rose>
+print 
