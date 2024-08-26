059bd6fa90380bb58178b26b9107ea59de23c7a5
tika2011 <indira.gorgadze.2017@gmail.com>
2024-08-27 00:03:04 +0400

    commit

---
+++ level 10/homework/lesson10.py
@@ -0,0 +1,51 @@
+# homework 3
+# > მეტობის ცვლადი
+print(123  > 321)
+print(988  > 779)
+print(781  > 921)
+print(1234 > 1224)
+print(112  > 1122)
+print(6759 > 6758)
+print(1213 > 1223)
+print(2112 > 1221)
+print(4454 > 8888)
+print(6218 > 2168)
+
+# < ნაკლებობის კომპონენტი
+print(123 < 81266)
+print(1212 < 3434)
+print(9009 < 8008)
+print(777 < 77777)
+print(6767 < 9099)
+print(234  < 4566)
+print(2017 < 2024)
+print(2011 < 1995)
+print(2023 < 2024)
+print(99 < 999999)
+
+# == შედარების კომპონენტი
+print(1112 == 1112)
+print(8899 == 8999)
+print(773 == 773)
+print(675 ==666)
+print(541 == 541)
+print(90 == 99)
+print(676 == 676)
+print(3434 == 3443)
+print(892 == 892)
+print(6789 == 76890)
+
+# homework 4
+# and rules
+print(34 > 24 and 56 > 27)
+print(45 > 12 and 777 > 778)
+print(987 > 988 and 899 > 667)
+print(999 > 876 and  777 > 889)
+print(555 == 555 and 987 > 333)
+
+# or rules
+print(66 < 88 or 76 < 98)
+print(44 < 22 or 67 < 44)
+print(87 < 54 or 88 < 94)
+print(97 < 98 or 67 < 68)
+print(88 == 89 or 77 == 89)
