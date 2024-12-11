edf4278051872e16d04b8e799c0aeab733a2a096
tika2011 <indira.gorgadze.2017@gmail.com>
2024-12-11 21:17:43 +0400

    commit

---
+++ level 24/clasework/lesson24 .py
@@ -0,0 +1,22 @@
+#clasework1
+ls1=[12,10,8,4,4]
+ls1[3]=6
+print(ls1)
+#clasework2
+nume=["a","n","a","s","t","a","s","i","a"]
+print(nume[1])
+#clasework3
+x="science"
+print (x[4] + x[2] + x[5] + x[6])
+#clasework4
+nums = [10,20,30,40,50,60, 70]
+print(nums[0:3])
+#claswork5
+nums = [10,20,30,40,50,60, 70]
+print(nums[5:7])
+#clasework6
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print=(nums[0:10])
+#clasework7
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[3:7])
