a0f3437b464aa37396beda22231a80682f2b5113
tika2011 <indira.gorgadze.2017@gmail.com>
2025-02-08 17:54:42 +0400

    commit

---
+++ level 28/homework/lesson28 .py
@@ -0,0 +1,17 @@
+#homework1
+def double(number):
+    return number * 2
+print(double(7))
+
+#homework2
+def double(number):
+    # ამით ვაორმაგებთ
+    return number * 2
+
+# print ფუნქცია გამოიყენება მისთვის რომ ვნახოთ შდეგი ტერმინალში
+print(double(8)) 
+ # პასუხი იქნება 16
+ #ფუნქცია ასრულებს გარკვეულ სამუშაოს
+ 
+ #homework3
+ 
