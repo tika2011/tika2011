6da59b1a2d87b5eae5e55247296687bf0d7a86bc
tika2011 <indira.gorgadze.2017@gmail.com>
2025-03-10 21:12:40 +0400

    commit

---
+++ level 38/clasework/lesson 38.py
@@ -0,0 +1,18 @@
+#clasework1
+def add_numbers(num1, num2):
+    return num1 + num2
+result = add_numbers(6, 3)
+print(result) 
+
+#clasework2
+def square_number(num):
+    return num * num
+result = square_number(6)
+print(result)
+
+#clasework3
+def greet(name):
+    print(f"Hello {name}")
+greet("Tika")
+#clasework4
+#ვერ გავიგე
---
+++ level 38/homework/lesson 38.py
@@ -0,0 +1,50 @@
+#homework1
+city = "tbilisi"
+print(city.upper()) 
+
+#homework2
+name = input("Tika")
+print(name.upper()) 
+
+#homework3
+text = "hello world"
+print(text.upper())
+
+#homework4
+name = "GABRIEL"
+print(name.lower())
+
+#homework5
+text = input("I love my world")
+print(text.lower())
+
+#homework6
+text = "PYTHON IS FUN"
+print(text.lower())
+
+#homework7
+name = "davit"
+print(name.capitalize())
+
+#homework8
+text = input("GOA")
+print(text.capitalize())
+
+#homework9
+text = "hello world"
+print(text.capitalize())
+
+#homework10
+text = "programming"
+position = text.find("g")
+print(position)
+
+#homework11
+text = "hello world"
+position = text.find("world")
+print(position)
+
+#homework12
+text = "python is fun"
+position = text.find("i")
+print(position)
